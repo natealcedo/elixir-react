93560502cefccd213ca7fdb1054d29e5f1d2b0ab
Nathaniel D Alcedo Jr <nathaniel.alcedo@axa.com.sg>
2018-05-03 21:12:14 +0800

    Complete tutorial until GET /blogs is implemented

---
+++ web/controllers/BlogsController.ex
@@ -0,0 +1,16 @@
+defmodule PhoenixReactCuratedList.BlogsController do
+  use PhoenixReactCuratedList.Web, :controller
+  alias PhoenixReactCuratedList.Blogs
+
+  def index(conn, _params) do
+    blogs = Repo.all(Blogs)
+    render conn, "index.json", blogs: blogs
+  end
+
+  def create(conn, %{"blogs" => blogs_params}) do
+  end
+
+  def show(conn, %{"id" => id}) do
+
+  end
+end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,9 @@ defmodule PhoenixReactCuratedList.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/blogs", BlogsController, :index
+    post "/blogs", BlogsController, :create
+    get "/blogs/:id", BlogsController, :show
   end
 
   # Other scopes may use custom stacks.
